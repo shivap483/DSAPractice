79ff882ad836a4062a0a540679ac92f166c5033d
Shiva Prasad <shivap483@gmail.com>
2021-05-22 10:19:26 +0530

    test commit

---
+++ run.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+from solution import Solution
+
+A = ".x.x.x..x"
+B = 66618
+C = 2820229
+test = Solution()
+s = datetime.now()
+print(test.seats(A))
+e = datetime.now()
+print(e - s)
+exit()
